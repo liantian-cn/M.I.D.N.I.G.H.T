a771c8e6e102730c394d3eb4db84277e752fe1be
liantian <liantian.me+code@gmail.com>
2026-03-07 18:14:57 +0800

    1

--- DejaVu/05_slots/99_test.lua
+++ DejaVu/05_slots/99_test.lua
@@ -1,7 +1,5 @@
-local addonName, addonTable = ...
+local _, addonTable = ...
 local COLOR = addonTable.COLOR
-local Slots = addonTable.Slots
-local Cell = addonTable.Cell
 local MegaCell = addonTable.MegaCell
 local BadgeCell = addonTable.BadgeCell
 local InitUI = addonTable.Event.Func.InitUI -- 初始化 UI 函数列表
@@ -13,4 +11,4 @@ local function TestSlot()
     local megaCell = MegaCell:New(12, 12)
     megaCell:setCell(136243)
 end
-table.insert(InitUI, TestSlot) -- 第二帧创建面板
+table.insert(InitUI, TestSlot) -- 初始化时创建测试槽位
