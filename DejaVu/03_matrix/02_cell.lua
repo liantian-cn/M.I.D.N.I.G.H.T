eb580229a9387b733e6cacce0fafd65f34919ecc
liantian <liantian.me+code@gmail.com>
2026-03-07 09:26:50 +0800

    x

--- DejaVu/03_matrix/02_cell.lua
+++ DejaVu/03_matrix/02_cell.lua
@@ -8,4 +8,38 @@
   draft
 ]]
 
-local addonName, addonTable = ...
+local addonName, addonTable = ... -- 插件名称与共享表
+local COLOR = addonTable.COLOR
+
+function addonTable.CreateCell(x, y, backgroundColor)
+    if not addonTable.Matrix.MartixFrame then
+        return
+    end
+    if not backgroundColor then
+        backgroundColor = COLOR.BLACK
+    end
+    local cell = {}
+    local parent = addonTable.Matrix.MartixFrame
+    local cellSize = addonTable.Matrix.SIZE.CELL
+    local cellSlug = x .. "_" .. y
+    local cellName = addonName .. "Cell_" .. cellSlug
+    local cellFrame = CreateFrame("Frame", cellName, parent)
+    cellFrame:SetPoint("TOPLEFT", parent, "TOPLEFT", x * cellSize, -y * cellSize)
+    cellFrame:SetFrameLevel(parent:GetFrameLevel() + 1)
+    cellFrame:SetSize(cellSize, cellSize)
+    cellFrame:Show()
+    local cellTexture = cellFrame:CreateTexture(nil, "BACKGROUND")
+    cellTexture:SetAllPoints(cellFrame)
+    cellTexture:SetColorTexture(backgroundColor:GetRGBA())
+    cellTexture:Show()
+    cell.Texture = cellTexture
+    cell.Frame = cellFrame
+    cell.Slug = cellSlug
+    cell.X = x
+    cell.Y = y
+    local function setColor(color)
+        cell.Texture:SetColorTexture(color:GetRGBA())
+    end
+    cell.setColor = setColor
+    -- return cellTexture, cellFrame
+end
