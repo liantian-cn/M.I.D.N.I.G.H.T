03b6b2bf27ac9558b0b217de9b738de1cdbd5ee1
alex <cybergeeker@outlook.com>
2026-04-20 11:52:54 +0800

    Add spell_stop_blacklist: stop casting when target casts blacklisted spells

--- Terminal/terminal/context/context.py
+++ Terminal/terminal/context/context.py
@@ -158,6 +158,10 @@ class Context:
         return self.decoded_data["range_spell_stop_list"]
 
     @property
+    def spell_stop_blacklist(self) -> list[str]:
+        return self.decoded_data['spell_stop_blacklist']
+
+    @property
     def spell_queue_window(self) -> float:
         return self.decoded_data["spell_queue_window"]
 
