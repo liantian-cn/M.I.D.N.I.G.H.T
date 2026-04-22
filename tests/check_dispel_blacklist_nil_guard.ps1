eb35dbdec774742dc6d7647914e852ef2b0fab36
liantian <liantian.me+code@gmail.com>
2026-04-23 00:23:39 +0800

    1

--- tests/check_dispel_blacklist_nil_guard.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$content = Get-Content -Raw -Encoding UTF8 "DejaVu/DejaVu_Common/DispelBlacklist.lua"
-
-if ($content -notmatch 'local function updateCell\(tableValue\)\s*[\r\n]+\s*tableValue = tableValue or \{\}\s*[\r\n]+\s*local i = 1') {
-    throw "DB_MISSING_NIL_GUARD"
-}
-
-Write-Host "check_dispel_blacklist_nil_guard.ps1: ok"
--- tests/check_spell_stop_list_nil_guard.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$content = Get-Content -Raw -Encoding UTF8 "DejaVu/DejaVu_Common/SpellStopList.lua"
-
-if ($content -notmatch 'local function updateCell\(tableValue\)\s*[\r\n]+\s*tableValue = tableValue or \{\}\s*[\r\n]+\s*local i = 1') {
-    throw "SSL_MISSING_NIL_GUARD"
-}
-
-Write-Host "check_spell_stop_list_nil_guard.ps1: ok"
