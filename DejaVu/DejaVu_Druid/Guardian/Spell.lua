1db509811d628e52ad1f86b044626f9e30b3cbd2
liantian <liantian.me+code@gmail.com>
2026-04-25 00:43:18 +0800

    1

--- DejaVu/DejaVu_Druid/Guardian/Spell.lua
+++ DejaVu/DejaVu_Druid/Guardian/Spell.lua
@@ -19,7 +19,7 @@ local DejaVu = _G["DejaVu"]
 local cooldownSpells = DejaVu.cooldownSpells
 local chargeSpells = DejaVu.chargeSpells
 
-
+DejaVu.useCustomSpell = true
 
 
 insert(cooldownSpells, { spellID = 102793, name = "乌索尔旋风" }) --  [乌索尔旋风]
