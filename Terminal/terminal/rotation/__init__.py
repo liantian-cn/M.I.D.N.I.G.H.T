5755df3e966a5cd61e60723aa15ce2fbde55edfd
liantian-cn <liantian.me+code@gmail.com>
2026-07-16 23:44:50 +0800

    Deprecated

--- Terminal/terminal/rotation/__init__.py
+++
@@ -1,9 +0,0 @@
-from .DruidGuardian import DruidGuardian
-from .DeathKnightBlood import DeathKnightBlood
-from .DruidRestoration import DruidRestoration
-from .PriestDiscipline import PriestDiscipline
-from .DruidGuardianElune import DruidGuardianElune
-from .DruidGuardian57 import DruidGuardian57
-
-
-ALL_ROTATIONS = [PriestDiscipline, DruidGuardian57, DeathKnightBlood, DruidRestoration, DruidGuardianElune, DruidGuardian]
